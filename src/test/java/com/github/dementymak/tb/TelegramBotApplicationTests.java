432e3141f52cc2ab41502062692b020c8d2398b2
Dementy <dementymak@gmail.com>
2022-08-31 09:43:40 +0200

    TB-0: added SpringBoot skeleton to the project.

---
+++ src/test/java/com/github/dementymak/tb/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dementymak.tb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

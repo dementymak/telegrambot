7710e46fd9e41c351d69cb3933a2b8b496ce8da1
dementymak <64837928+dementymak@users.noreply.github.com>
2022-09-20 11:28:56 +0200

    TB-2: added stab telegram bot to the project. (#17)
    * TB-2: added stab telegram bot to the project.
    
    * TB-2: updated project version and added to RELEASE_NOTES

--- src/main/java/com/github/dementymak/tb/TelegramBotApplication.java
+++ src/main/java/com/github/dementymak/tb/TelegramBotApplication.java
@@ -2,7 +2,10 @@ package com.github.dementymak.tb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+
+@EnableScheduling
 @SpringBootApplication
 public class TelegramBotApplication {
 

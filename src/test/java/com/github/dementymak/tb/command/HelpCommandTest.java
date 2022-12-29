fba7c07d4fe193d3a74684a2585f50cd658dd2f0
Dementy <dementymak@gmail.com>
2022-12-29 22:15:21 +0100

    TB-2: updated project version and added to RELEASE_NOTES

---
+++ src/test/java/com/github/dementymak/tb/command/HelpCommandTest.java
@@ -0,0 +1,25 @@
+package com.github.dementymak.tb.command;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static com.github.dementymak.tb.command.CommandName.HELP;
+import static com.github.dementymak.tb.command.HelpCommand.HELP_MESSAGE;
+
+@DisplayName("Unit-level testing for HelpCommand")
+public class HelpCommandTest extends AbstractCommandTest {
+
+    @Override
+    String getCommandName() {
+        return HELP.getCommandName();
+    }
+
+    @Override
+    String getCommandMessage() {
+        return HELP_MESSAGE;
+    }
+
+    @Override
+    Command getCommand() {
+        return new HelpCommand(sendBotMessageService);
+    }
+}

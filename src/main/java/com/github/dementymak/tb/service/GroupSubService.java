aa8b18af6d202dbd6e4985a9c59648a1671e8b15
Dementy <dementymak@gmail.com>
2023-01-18 19:14:18 +0100

    STEP_7 TB-7: added ability to delete group subscription.

--- src/main/java/com/github/dementymak/tb/service/GroupSubService.java
+++ src/main/java/com/github/dementymak/tb/service/GroupSubService.java
@@ -3,10 +3,14 @@ package com.github.dementymak.tb.service;
 import com.github.dementymak.tb.javarushclient.dto.GroupDiscussionInfo;
 import com.github.dementymak.tb.repository.entity.GroupSub;
 
+import java.util.Optional;
+
 /**
  * Service for manipulating with {@link GroupSub}.
  */
 public interface GroupSubService {
 
     GroupSub save(String chatId, GroupDiscussionInfo groupDiscussionInfo);
+    GroupSub save(GroupSub groupSub);
+    Optional<GroupSub> findById(Integer id);
 }

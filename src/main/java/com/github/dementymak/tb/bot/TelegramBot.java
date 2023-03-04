ae7b793dd2406000cb6f50cb78c7ab4b5a80a79b
Dementy <dementymak@gmail.com>
2023-03-04 23:04:37 +0100

    STEP_10 TB-10: added AdminCommand annotation and AdminHelpCommand.

--- src/main/java/com/github/dementymak/tb/bot/TelegramBot.java
+++ src/main/java/com/github/dementymak/tb/bot/TelegramBot.java
@@ -3,6 +3,7 @@ package com.github.dementymak.tb.bot;
 import com.github.dementymak.tb.command.CommandContainer;
 import com.github.dementymak.tb.javarushclient.JavaRushGroupClient;
 import com.github.dementymak.tb.service.GroupSubService;
+import com.github.dementymak.tb.service.StatisticsService;
 import com.github.dementymak.tb.service.SendBotMessageServiceImpl;
 import com.github.dementymak.tb.service.TelegramUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import java.util.List;
 
 import static com.github.dementymak.tb.command.CommandName.NO;
 
@@ -27,20 +29,23 @@ public class TelegramBot extends TelegramLongPollingBot {
     private final CommandContainer commandContainer;
 
     @Autowired
-    public TelegramBot(TelegramUserService telegramUserService, JavaRushGroupClient groupClient, GroupSubService groupSubService) {
-        this.commandContainer = new CommandContainer(new SendBotMessageServiceImpl(this), telegramUserService, groupClient, groupSubService);
+    public TelegramBot(TelegramUserService telegramUserService, JavaRushGroupClient groupClient, GroupSubService groupSubService,
+                               @Value("#{'${bot.admins}'.split(',')}") List<String> admins, StatisticsService statisticsService) {
+        this.commandContainer =
+                new CommandContainer(new SendBotMessageServiceImpl(this),
+                        telegramUserService, groupClient, groupSubService, admins, statisticsService);
     }
 
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String message = update.getMessage().getText().trim();
+            String username = update.getMessage().getFrom().getUserName();
             if (message.startsWith(COMMAND_PREFIX)) {
                 String commandIdentifier = message.split(" ")[0].toLowerCase();
-
-                commandContainer.retrieveCommand(commandIdentifier).execute(update);
+                commandContainer.retrieveCommand(commandIdentifier, username).execute(update);
             } else {
-                commandContainer.retrieveCommand(NO.getCommandName()).execute(update);
+                commandContainer.retrieveCommand(NO.getCommandName(), username).execute(update);
             }
         }
     }
--- src/test/java/com/github/dementymak/tb/command/StatCommandTest.java
+++ src/test/java/com/github/dementymak/tb/command/StatCommandTest.java
@@ -1,21 +1,53 @@
 package com.github.dementymak.tb.command;
 
+import com.github.dementymak.tb.dto.GroupStatDTO;
+import com.github.dementymak.tb.dto.StatisticDTO;
+import com.github.dementymak.tb.service.SendBotMessageService;
+import com.github.dementymak.tb.service.StatisticsService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Collections;
+
+import static com.github.dementymak.tb.command.AbstractCommandTest.prepareUpdate;
 import static com.github.dementymak.tb.command.CommandName.STAT;
 import static com.github.dementymak.tb.command.StatCommand.STAT_MESSAGE;
+import static java.lang.String.format;
 
-public class StatCommandTest extends AbstractCommandTest {
-    @Override
-    String getCommandName() {
-        return STAT.getCommandName();
+
+@DisplayName("Unit-level testing for StatCommand")
+public class StatCommandTest {
+
+    private SendBotMessageService sendBotMessageService;
+    private StatisticsService statisticsService;
+    private Command statCommand;
+
+    @BeforeEach
+    public void init() {
+        sendBotMessageService = Mockito.mock(SendBotMessageService.class);
+        statisticsService = Mockito.mock(StatisticsService.class);
+        statCommand = new StatCommand(sendBotMessageService, statisticsService);
     }
 
-    @Override
-    String getCommandMessage() {
-        return String.format(STAT_MESSAGE, 0);
-    }
+    @Test
+    public void shouldProperlySendMessage() {
+        //given
+        Long chatId = 1234567L;
+        GroupStatDTO groupDto = new GroupStatDTO(1, "group", 1);
+        StatisticDTO statisticDTO = new StatisticDTO(1, 1, Collections.singletonList(groupDto), 2.5);
+        Mockito.when(statisticsService.countBotStatistic())
+                .thenReturn(statisticDTO);
 
-    @Override
-    Command getCommand() {
-        return new StatCommand(sendBotMessageService, telegramUserService);
+        //when
+        statCommand.execute(prepareUpdate(chatId, CommandName.STAT.getCommandName()));
+
+        //then
+        Mockito.verify(sendBotMessageService).sendMessage(chatId.toString(), format(STAT_MESSAGE,
+                statisticDTO.getActiveUserCount(),
+                statisticDTO.getInactiveUserCount(),
+                statisticDTO.getAverageGroupCountByUser(),
+                format("%s (id = %s) - %s подписчиков", groupDto.getTitle(), groupDto.getId(), groupDto.getActiveUserCount())));
     }
 }
